984add4f278b63faa8ccfbedc0467f4804ead93f
Yassine Lafkih <yassine.lf.99@gmail.com>
2023-11-13 11:04:07 +0000

    init test

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+'''
+EN------------------------------------------------------------------
+we hava a list LI = [2, 8, 100, 50, 3, 8, 20] and a target number N = 120
+write an algorithm that takes a list of digits and return two indices list of two sum numbers that will match a target number N
+f(arg:LI) --> ret:[i, j]
+
+FR------------------------------------------------------------------
+nous avons une liste LI = [2, 8, 100, 50, 3, 8, 20] et un nombre cible N = 120
+écrire un algorithme qui prend une liste des entiers et renvoie deux indices, une liste de deux nombres de somme qui correspondent à un nombre cible N
+f(arg:LI) --> ret:[i, j]
+
+'''
+
+def two_sum():
+    raise NotImplementedError
+
+if __name__ == '__main__':
+    two_sum()
